a251b97e32baf58472997dd9d2a9d206d3ef9397
Naveen Kumar <naveenkrgupat7234@gmail.com>
2021-01-08 00:21:29 +0530

    Initial Commit

---
+++ View/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  View
+//
+//  Created by Mr. Naveen Kumar on 08/01/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func backBtn(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ View/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  View
+//
+//  Created by Mr. Naveen Kumar on 07/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+    @IBAction func clickMe(_ sender: UIButton) {
+        print("H")
+        let vc = SecondViewController()
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: true, completion: nil)
+        
+    }
+}
+
